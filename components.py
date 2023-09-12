ef34df68b05c5f3eb9dfc1b47960bd88da7e2459
Lan H Mai <lanmai@udel.edu>
2023-09-12 13:38:21 -0400

    Add waveguide cells

--- components.py
+++ components.py
@@ -23,3 +23,19 @@ def UDNF_10mm_all_markers() -> gf.Component:
 @gf.cell
 def labels() -> gf.Component:
     return
+
+@gf.cell
+def inverse_design_coupler_wvg() -> gf.Component:
+    return
+
+@gf.cell
+def electrical_contacts() -> gf.Component:
+    return
+
+@gf.cell
+def grating_coupler() -> gf.Component:
+    return
+
+@gf.cell
+def photonic_crystal_wvg() ->gf.Component:
+    return
